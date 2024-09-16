b3b19b654d64b8311826d0acbb83ca2ea30b35e9
cortinovis.21123 <cortinovis.matteo.studente88@itispaleocapa.it>
2024-09-16 13:19:08 +0200

    inserimento descrizioni funzioni

--- src/Main.java
+++ src/Main.java
@@ -1,3 +1,5 @@
+package esconcessionario;
+
 public class Main {
     public static void main(String[] args) {
 
--- src/funzionii.java
+++ src/funzionii.java
@@ -1,2 +1,38 @@
+package esconcessionario;
+
 public class funzionii {
+
+    //aggiunta di una nuova auto
+    public int aggiunta(String [] marca, String[] modello, int [] prezzo, String maarca, int preezzo, String moodello, int num){
+        num=0;
+        marca[num]=maarca;
+        modello[num]=moodello;
+        prezzo[num]=preezzo;
+        num=num+1;
+    }
+    //visualizzazione di tutte le auto
+    public String visualizzazione (String [] marca, String[] modello, int [] prezzo, int num){
+        num=0;
+        num=num+1;
+    }
+    //ricerca di un'auto tramite marca e modello;
+    public String ricerca(){
+
+    }
+    //cancellazione di un'auto;
+    public String cancellazione(){
+
+    }
+    //modifica dei dati di un'auto;
+    public String modifica(){
+
+    }
+    //visualizzazione delle auto in ordine crescente;
+    public String crescente(){
+
+    }
+    //visualizzare tutti i modelli che diesel;
+    public String diesel(){
+
+    }
 }
