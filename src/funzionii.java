2ac836fd4127a5626fada898a008bbe4632e7a0b
Cortinovis <160122402+shortinovis@users.noreply.github.com>
2024-09-23 13:24:22 +0200

    Update funzionii.java
    miglioramento

--- src/funzionii.java
+++ src/funzionii.java
@@ -3,44 +3,48 @@ package esconcessionario;
 public class funzionii {
 
     //aggiunta di una nuova auto
-    public int aggiunta(String [] marca, String[] modello, int [] prezzo, String maarca, int preezzo, String moodello, int num){
+    public static void aggiunta(String [] marca, String[] modello, double [] prezzo, String maarca, double preezzo, String moodello, int num){
         marca[num]=maarca;
         modello[num]=moodello;
         prezzo[num]=preezzo;
-        num++;
-        return num;
     }
     //visualizzazione di tutte le auto
-    public int visualizzazione (int num){
-        num++;
-        return num;
+    public static String visualizzazione(int num, String[] marca, String[] modello, double[] prezzo){
+        String macchina="";
+        for(int i=0; i<num; i++){
+            macchina= macchina+marca[i]+"\t"+modello[i]+"\t"+prezzo[i]+"\t\n";
+        }
+        return macchina;
     }
     //ricerca di un'auto tramite marca e modello;
-    public int ricerca(int num, String [] marca, String [] modello, String maarca, String moodello){
+    public static int ricerca( String [] marca, String [] modello, String maarca, String moodello){
         for(int i=0; i< modello.length; i++){
-            if(moodello==modello[i]){
-                num=i;
+            if(moodello==modello[i]&& maarca==marca[i]){
+                return i;
             }
         }
-        return num;
+        return -1;
     }
     //cancellazione di un'auto;
-    public void cancellazione(){
+    public static void cancellazione(){
 
     }
     //modifica dei dati di un'auto;
-    public void modifica(String modellonuovo, String marcanuova, String prezzonuovo, String [] marca, String [] modello, String [] prezzo, int num){
+    public static void modifica(int num,String modellonuovo, String marcanuova, double prezzonuovo, String [] marca, String [] modello, double [] prezzo){
         marca[num]=marcanuova;
         modello[num]=modellonuovo;
         prezzo[num]=prezzonuovo;
 
     }
     //visualizzazione delle auto in ordine crescente;
-    public String crescente(){
+    public static String crescente(){
 
     }
     //visualizzare tutti i modelli che diesel;
-    public String diesel(){
-
+    public static String diesel( String [] marca, String [] modello, double [] prezzo, int num){
+        String diesel="";
+        for(int i=0;i<num;i++){
+            String [] paole =modello
+        }
     }
 }
